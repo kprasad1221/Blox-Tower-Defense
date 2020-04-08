0b6f4c390dfb97021b1ad1c60bfc02f6b8d1c5ab
kprasad1221 <kprasad.1221@gmail.com>
2020-04-07 20:01:26 -0800

    setup grid snap in editor script

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(5f, 20f)] [SerializeField] float snapSize = 10f;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / snapSize) * snapSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / snapSize) * snapSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
