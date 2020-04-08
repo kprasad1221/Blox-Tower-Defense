7c1ee458e990569eeb70fa455714026d61a12c0b
kprasad1221 <kprasad.1221@gmail.com>
2020-04-07 22:38:20 -0800

    built smart label text mesh system

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+
+public class CubeEditor : MonoBehaviour
 {
     [Range(5f, 20f)] [SerializeField] float snapSize = 10f;
 
+    TextMeshPro textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMeshPro>();
+        print(textMesh);
+        textMesh.text = "test";
+    }
+
     void Update()
+    {
+        SnapBoxToGrid();
+    }
+
+    private void SnapBoxToGrid()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / snapSize) * snapSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / snapSize) * snapSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+        textMesh.text = snapPos.x / snapSize + " , " + snapPos.z / snapSize;
     }
 }
